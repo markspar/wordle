d37d1c5ffabf53a55da753048332d273082fa080
markspar <31252279+markspar@users.noreply.github.com>
2022-02-10 18:44:22 -0500

    Update wordlegame.ps1
    updated to 1.0

--- wordlegame.ps1
+++ wordlegame.ps1
@@ -1,26 +1,47 @@
-﻿# get starting word
-$wordnikapikey = 'blahblah'
-$wordlen = 5
+# get starting word
+$wordnikapikey = 'putyourapikeyhere'
+$wordlen = '5'
 $wordnikurl = 'https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength='+$wordlen+'&maxLength='+$wordlen+'&api_key='+$wordnikapikey
-#try 
-#{
-#    $r = Invoke-WebRequest -Uri $wordnikurl -Method Get
-#    $result = ConvertFrom-Json -InputObject $r.Content
-#}
-#catch 
-#{
-#    throw $_
-#}
-# write-output $result
-$answer='BADLY'
+
+try 
+{
+    $r = Invoke-WebRequest -Uri $wordnikurl -Method Get
+    $result = ConvertFrom-Json -InputObject $r.Content
+}
+catch 
+{
+    throw $_
+}
+$answer=$result[0].word
 
 # loop for 6 guesses
 $l=1
+Clear-Host
 DO {
     # get guess word
     do {
         $guess = Read-Host "Guess $($l)"
         # validate guess is a word
+        $wordnikurl = 'https://api.wordnik.com/v4/word.json/'+$guess+'/definitions?limit=200&includeRelated=false&sourceDictionaries=all&useCanonical=true&includeTags=false&api_key='+$wordnikapikey
+        if ($guess.length -eq 5) {
+            try 
+            {
+                $r = Invoke-WebRequest -Uri $wordnikurl -Method Get
+                $result = ConvertFrom-Json -InputObject $r.Content
+                if ($r.StatusCode -ne 200) { 
+                    $guess = $null 
+                    write-output "That wasn't a word!"
+                }
+            }
+            catch 
+            {
+                $guess = $null
+                write-output "That wasn't a word!"
+            }
+        }
+        else {
+            write-output "That wasn't 5 characters."
+        }
     }
     while ($guess.length -ne 5)
 
@@ -70,4 +91,5 @@ DO {
     $l++
 } while ($l -le 6)
 
+if ($guess -ne $answer) { write-output "The word was $($answer)!" }
 write-host "Thanks for playing!"
